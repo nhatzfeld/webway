048051dd38506acf857471072b1e7dd0046b6786
Nathan <n.hatz@yahoo.com>
2018-03-26 12:33:55 -0500

    Typos fixed

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const cors = require("cors");
 const { json } = require("body-parser");
 const massive = require("massive");
 
+massive(process.env.CONNECTION_STRING)
+  .then(db => {
+    app.set("db", db);
+  })
+  .catch(err => console.log("Massive: CONNECTION_STRING_ERROR!!!"));
+
 const app = express();
 
 app.use(cors());
